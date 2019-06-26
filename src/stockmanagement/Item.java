6a52ba5b7b2b5c6c73111d090cf8fb8b6b4d7957
Yasas <Yasas@DESKTOP-GDVEFHB>
2019-06-26 22:18:37 +0530

    Dashboard and Item class

---
+++ src/stockmanagement/Item.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockmanagement;
+
+/**
+ *
+ * @author Yasas
+ */
+public class Item {
+    int ID;
+    String type;
+    int quantity;
+    float price;
+    
+    
+    
+}
