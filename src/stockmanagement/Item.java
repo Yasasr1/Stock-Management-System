af80f311dd6cf34d0c89d5d06cec8689661468a6
Yasas <Yasas@DESKTOP-GDVEFHB>
2019-06-28 09:47:14 +0530

    getter methods

--- src/stockmanagement/Item.java
+++ src/stockmanagement/Item.java
@@ -10,10 +10,61 @@ package stockmanagement;
  * @author Yasas
  */
 public class Item {
-    int ID;
+    String item_code;
     String type;
+    String name;
     int quantity;
+    String supplier;
+    int reorder_level;
     float price;
+    
+    Item(String item_code,String type,String name,int quantity,String supplier,int reorder_level,float price)
+    {
+        this.item_code = item_code;
+        this.type = type;
+        this.name = name;
+        this.quantity = quantity;
+        this.supplier = supplier;
+        this.reorder_level = reorder_level;
+        this.price = price;
+        
+        
+    }
+    
+    String getCode()
+    {
+        return this.item_code;
+    }
+    
+    String getType()
+    {
+        return this.type;
+    }
+    
+    String getName()
+    {
+        return this.name;
+    }
+    
+    int getquantity()
+    {
+        return this.quantity;
+    }
+    
+    String getSupplier()
+    {
+        return this.supplier;
+    }
+    
+    int getReLevel()
+    {
+        return this.reorder_level;
+    }
+    
+    float getUnPrice()
+    {
+        return this.price;
+    }
     
     
     
